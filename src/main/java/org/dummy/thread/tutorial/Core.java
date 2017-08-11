528e9d1efc5aeabe2d92571f16ceddd1c3509511
davik3000 <davik3000@users.noreply.github.com>
2017-08-11 18:46:06 +0200

    WIP - first revision

---
+++ src/main/java/org/dummy/thread/tutorial/Core.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dummy.thread.tutorial;
+
+public class Core {
+
+    // http://winterbe.com/posts/2015/04/07/java8-concurrency-tutorial-thread-executor-examples/
+}
